2a6a2c25ff43d20158a007fd2c414be2a8f49911
chizakasipascal <pascalkasichiza@gmail.com>
2023-10-11 15:47:37 +0200

    refactoring code dto

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+    // eslint-disable-next-line prettier/prettier
     getHello(): string {
         return 'Hello World!';
     }
